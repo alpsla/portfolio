12954c8ceb319d60b46607804cbcd23578cbb9ee
anilbvi <bobby.anil@viacomcontractor.com>
2025-10-23 16:36:56 +0530

    fix(config-comparison): add SharePoint video link and WCTV platform tag

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -214,7 +214,9 @@ export const PROJECTS: IProject[] = [
     screenshots: [],
     status: 'draft',
     attachments: [],
-    links: [{ label: 'WCTV Config Report Generation Training Video', url: 'https://viacom.sharepoint.com/:v:/r/sites/VIA-Digital-QAConnectedTV/Shared%20Documents/Unified/CTV-Training/WCTV/20240125%20WCTV%20Config%20Report%20Generation%20for%20RC.mp4?csf=1&web=1&e=qiG0Sb', sensitivity: 'internal' },],
+    links: [
+      { label: 'WCTV Config Report Generation Training Video', url: 'https://viacom.sharepoint.com/:v:/r/sites/VIA-Digital-QAConnectedTV/Shared%20Documents/Unified/CTV-Training/WCTV/20240125%20WCTV%20Config%20Report%20Generation%20for%20RC.mp4?csf=1&web=1&e=qiG0Sb', sensitivity: 'internal' },
+    ],
     tags: [
       { key: 'platform:wctv', label: 'WCTV' },
     ],
